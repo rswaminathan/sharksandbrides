4255701f7ff5dc31c409f93ce7e079d2b63d5920
qxc <cold.ferret@gmail.com>
2012-04-11 20:05:08 -0700

    added my ps6 - rahul/create.php(richard)
    Signed-off-by: qxc <cold.ferret@gmail.com>

---
+++ ps6 - richard/index.php
@@ -0,0 +1,48 @@
+<!---
+Richard Truong
+40115583
+richard_truong@hmc.edu
+--->
+
+<body>
+
+<h1>PART A:</h1>
+<form id="parta" action="parta.php" method="post">
+    Price: $<input type='text' name='price' id ='price' size='40'>
+    <input type="submit" />
+</form>
+
+<h1>PART B:</h1>
+<form id="partb" action="partb.php" method="post">
+    <p>Min Speed: <input type='text' name='speed' id ='speed' size='40'>gHZ
+    <p>Min RAM: <input type='text' name='ram' id ='ram' size='40'>GB
+	<p>Min HD Size: <input type='text' name='hd' id ='hd' size='40'>GB
+    <p>Min Screen Size: <input type='text' name='screen' id ='screen' size='40'>inches
+    <p><input type="submit" />
+</form>
+
+<h1>PART C:</h1>
+<form id="partc" action="partc.php" method="post">
+    <p>Manufacturer (maker): <input type='text' name='maker' id ='maker' size='40'>
+    <input type="submit" />
+</form>
+
+<h1>PART D:</h1>
+<form id="partd" action="partd.php" method="post">
+    <p>Budget: $<input type='text' name='budget' id ='budget' size='40'>
+	<p>Min Speed: <input type='text' name='speed' id ='speed' size='40'>
+    <input type="submit" />
+</form>
+
+<h1>PART E:</h1>
+<form id="parte" action="parte.php" method="post">
+    <p>Manufacturer (maker): <input type='text' name='maker' id ='maker' size='40'>
+    <p>Model: <input type='text' name='model' id ='model' size='40'>
+    <p>Speed: <input type='text' name='speed' id ='speed' size='40'>
+    <p>RAM: <input type='text' name='ram' id ='ram' size='40'>
+	<p>HD Size <input type='text' name='hd' id ='hd' size='40'>
+	<p>Price <input type='text' name='price' id ='price' size='40'>
+    <input type="submit" />
+</form>
+
+</body>
