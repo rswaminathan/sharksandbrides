147bb6c684c19d38c68473beae1db760874cd6a0
Reket <reket1990@yahoo.com>
2012-04-25 17:01:12 -0700

    about and contact

---
+++ dbs - final project/about.php
@@ -0,0 +1,23 @@
+<div class="page-header">
+  <h1>About</h1>
+</div>
+
+<p>
+This website/store was created for CS133 - Database Systems, Final Project.
+</p>
+
+</br>
+
+<p>
+Our final project is a website in which customers can buy Sharks or Russian brides.  It has all the features of a typical website.  It has a login system, cart, checkout, wishlist, reviews, specials as well as many other features that a online store would have.
+</p>
+
+<p>
+In addition, our website also has an employee link which can be used to add items, put items on sale, as well as many other features that could be used to manage the website.
+</p>
+
+</br>
+
+<p>
+We would like to thank our professor Art Lee for teaching this class!
+</p>
