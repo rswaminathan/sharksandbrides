726f23625a9c729e4353e33ab14af1a1c5723d40
Reket <reket1990@yahoo.com>
2012-04-25 21:48:40 -0700

    asd

--- dbs - final project/buywebsite.php
+++ dbs - final project/buywebsite.php
@@ -1,3 +1,3 @@
-Buy the website.  Press the button and buy it for the low low price of 400 daily installments of $2500. Offer only valid until we feel like not having it be offered.
+Buy this website.  Click below to purchase for the low low price of 400 daily installments of $2500. <br> Offer only valid until we feel like not having it be offered.<br><br>
 
 <a href="/index.php/cart?item_id=-1&type=shark" class="btn btn-large">Purchase Website</a>
